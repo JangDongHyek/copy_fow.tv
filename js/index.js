b2ab566521f835e29bad8d8e78a63876e81b291e
JangDongHyek <JangDongHyek@gmail.com>
2018-12-13 23:43:34 +0900

    FOW.TV's Basics Js

---
+++ js/index.js
@@ -0,0 +1,53 @@
+$("#btn_home").click(function() {
+  alert("처음으로");
+});
+
+$("#btn_write").click(function() {
+  alert("영상등록");
+});
+
+$("#btn_back").click(function() {
+  alert("back");
+});
+
+$("#btn_next").click(function() {
+  alert("next");
+});
+
+$(".class_menu").click(function() {
+  alert("메뉴들");
+});
+
+// ////////////////////////////////////////////////////////////////////////////
+$("#like_btn").click(function() {
+  alert("좋아요");
+});
+
+$("#dislike_btn").click(function() {
+  alert("싫어요");
+});
+
+$("#movie").click(function() {
+  alert("영상");
+});
+
+$("#ut").click(function() {
+  alert("유저제목");
+});
+
+$("#yt").click(function() {
+  alert("유툽제목");
+});
+
+$("#co").click(function() {
+  alert("댓글");
+});
+
+$("#re").click(function() {
+  alert("신고");
+});
+
+/////////////////////////////////////////////////////////////////////////////
+$(".side_btn").click(function() {
+  alert("사이드버튼");
+});
